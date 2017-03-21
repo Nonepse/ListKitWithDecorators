7ff6dae49b179a7281b35c8297d1d326e4c6d4b5
Pierre Espenan <pierre.espenan@gmail.com>
2017-03-21 15:20:30 +0000

    Create Xcode project

---
+++ ListKitWithDecorator/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ListKitWithDecorator
+//
+//  Created by Pierre Espenan on 21/03/2017.
+//  Copyright © 2017 Pierre Espenan. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+class FeedViewController: UIViewController, IGListAdapterDataSource {
+
+}
